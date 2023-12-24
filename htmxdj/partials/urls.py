f1c4d1035e42bc75f59fc8c978b6b01832a7a10e
Jonas Huber <jonas.huber@rl-institut.de>
2023-12-24 10:56:21 +0100

    add urls for partial pages

--- htmxdj/partials/urls.py
+++ htmxdj/partials/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from htmxdj.partials.views import PartialHomeProjects
+from htmxdj.partials.views import PartialHomeProjects, PartialHomeServices, PartialHomeNews
 
 app_name = "users"
 urlpatterns = [
     path("projects/", view=PartialHomeProjects.as_view(), name="partial-projects"),
-    path("services/", view=PartialHomeProjects.as_view(), name="partial-services"),
-    path("news/", view=PartialHomeProjects.as_view(), name="partial-news"),
+    path("services/", view=PartialHomeServices.as_view(), name="partial-services"),
+    path("news/", view=PartialHomeNews.as_view(), name="partial-news"),
 ]
